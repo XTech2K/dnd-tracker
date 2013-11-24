5b84c3c41dc07ef026010abdb3e8c5c6a9de8a63
XTech2K <xander@kimxtom.com>
2013-11-24 11:25:47 -0500

    add simple http authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :authenticate
+  protected
+  def authenticate
+    auth = ENV['DND_TRACKER_AUTH']
+    if auth
+      auth_array = auth.split(':')
+      authenticate_or_request_with_http_basic do |username, password|
+        username == auth_array[0] && password == auth_array[1]
+      end
+    else
+      true
+    end
+  end
 end
