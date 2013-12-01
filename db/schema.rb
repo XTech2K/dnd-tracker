c61f99bd227c1fa7ec6c083ff50ecc96bf163417
XTech2K <xander@kimxtom.com>
2013-12-01 11:22:34 -0500

    added fields to character model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131201143003) do
+ActiveRecord::Schema.define(version: 20131201162001) do
 
   create_table "characters", force: true do |t|
     t.string   "name"
